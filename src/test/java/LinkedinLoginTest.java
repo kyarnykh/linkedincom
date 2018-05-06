3cdea9016ae5261e4aae18fea584c2ac3b81ecfc
kiarn <k.iarnykh@gmail.com>
2018-05-07 01:03:50 +0300

    Homework 4

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -28,11 +28,11 @@ public class LinkedinLoginTest {
                 "Title is missing");
 
         WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
-        String userLogin = "email@gmail.com";
+        String userLogin = "correctEmail@gmail.com";
         userEmailField.sendKeys(userLogin);
 
         WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
-        String userPassword = "Password";
+        String userPassword = "correctPassword";
         userPasswordField.sendKeys(userPassword);
 
         WebElement signInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
@@ -44,21 +44,113 @@ public class LinkedinLoginTest {
                 "Title is missing");
     }
 
-     @Test
-        public void verifyLoginWithEmptyUsernameAndPassword() throws InterruptedException {
-         WebElement singInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
-         Assert.assertTrue(singInButton.isDisplayed(),
-                 "Sing In button is missing");
+    @Test
+    public void successfulCaseIntensiveLogin() throws InterruptedException {
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Войти или зарегистрироваться",
+                "Title is missing");
+        WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        String userLogin = "CORRECTEMAILL@gmail.com";
+        userEmailField.sendKeys(userLogin);
 
-         WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
-         userEmailField.sendKeys("");
+        WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        String userPassword = "correctPassword";
+        userPasswordField.sendKeys(userPassword);
 
-         WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
-         userPasswordField.sendKeys("");
+        WebElement signInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+        signInButton.click();
 
-         singInButton.click();
-         sleep(5000);
-         Assert.assertTrue(singInButton.isDisplayed(), "Sing In button is missing");
+        sleep(5000);
+
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn",
+                "Title is missing");
+    }
+
+    @Test
+    public void verifyLoginWithEmptyUsernameAndPassword() throws InterruptedException {
+        WebElement singInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+        Assert.assertTrue(singInButton.isDisplayed(),
+                "Sing In button is missing");
+
+        WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        userEmailField.sendKeys("");
+
+        WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        userPasswordField.sendKeys("");
+
+        singInButton.click();
+        sleep(5000);
+        Assert.assertTrue(singInButton.isDisplayed(), "Sing In button is missing");
      }
+
+    @Test
+    public void verifyLoginWithEmptyUsername() throws InterruptedException {
+        WebElement singInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+        Assert.assertTrue(singInButton.isDisplayed(),
+                "Sing In button is missing");
+
+        WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        userEmailField.sendKeys("");
+
+        WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        userPasswordField.sendKeys("correctPassword");
+
+        singInButton.click();
+        sleep(5000);
+        Assert.assertTrue(singInButton.isDisplayed(), "Sing In button is missing");
+    }
+
+    @Test
+    public void verifyLoginWithEmptyPassword() throws InterruptedException {
+        WebElement singInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+        Assert.assertTrue(singInButton.isDisplayed(),
+                "Sing In button is missing");
+
+        WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        userEmailField.sendKeys("correctEmail@gmail.com");
+
+        WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        userPasswordField.sendKeys("");
+
+        singInButton.click();
+        sleep(5000);
+        Assert.assertTrue(singInButton.isDisplayed(), "Sing In button is missing");
+    }
+
+    @Test
+    public void verifyLoginWithIncorrectUsername() throws InterruptedException {
+        WebElement singInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+        Assert.assertTrue(singInButton.isDisplayed(),
+                "Sing In button is missing");
+
+        WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        userEmailField.sendKeys("incorrectEmail@gmail.com");
+
+        WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        userPasswordField.sendKeys("correctPassword");
+
+        singInButton.click();
+        sleep(5000);
+        WebElement errorMessage = webDriver.findElement(By.xpath("//*[@id='session_key-login-error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is missing");
+    }
+
+    @Test
+    public void verifyLoginWithIncorrectPassword() throws InterruptedException {
+        WebElement singInButton = webDriver.findElement(By.xpath("//*[@id='login-submit']"));
+        Assert.assertTrue(singInButton.isDisplayed(),
+                "Sing In button is missing");
+
+        WebElement userEmailField = webDriver.findElement(By.xpath("//*[@id='login-email']"));
+        userEmailField.sendKeys("correctEmail@gmail.com");
+
+        WebElement userPasswordField = webDriver.findElement(By.xpath("//*[@id='login-password']"));
+        userPasswordField.sendKeys("incorrectPassword");
+
+        singInButton.click();
+        sleep(5000);
+        WebElement errorMessage = webDriver.findElement(By.xpath("//*[@id='session_password-login-error']"));
+        Assert.assertTrue(errorMessage.isDisplayed(), "Error message is missing");
+    }
+
 
 }
